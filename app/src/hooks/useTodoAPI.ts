d1c04ddd8220c56218e16febf00f36fe5dfd8a04
Ibrar talab <ibrartalabkhan@gmail.com>
2025-08-05 22:51:21 +0500

    create a useTodoApi hook to implment all todos logic as an abstraction layer.

--- app/src/hooks/useTodoAPI.ts
+++ app/src/hooks/useTodoAPI.ts
@@ -22,21 +22,25 @@ export interface AddTodoResponse {
 const useTodoApi = () => {
   const axiosPrivate = useAxiosPrivate();
 
+  // Add a new todo item
   const addTodo = useCallback(async (data: AddTodoInput) => {
     const response = await axiosPrivate.post<AddTodoResponse>("/Todo", data);
     return response.data;
   }, [axiosPrivate]);
 
+  // Get all todo items
   const getTodos = useCallback(async () => {
     const response = await axiosPrivate.get<AddTodoResponse[]>("/Todo");
     return response.data;
   }, [axiosPrivate]);
 
+  // Update an existing todo item
   const updateTodo = useCallback(async (id: number, data: UpdateTodoInput) => {
     const response = await axiosPrivate.put<void>(`/Todo/${id}`, data);
     return response;
   }, [axiosPrivate]);
 
+  // Delete a todo item
   const deleteTodo = useCallback(async (id: number) => {
     const response = await axiosPrivate.delete<void>(`/Todo/${id}`);
     return response.data;
